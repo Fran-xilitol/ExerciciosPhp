8600f6ad85b23e68a6f80a80199dc0e41c6836f6
Fran-xilitol <55721827+Fran-xilitol@users.noreply.github.com>
2020-08-14 19:53:46 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -34,44 +34,45 @@ class Resolucao implements TextWrapInterface {
     // Espaço disponível de cada linha.
     for ($i = 0; $i < count($words); $i++) {
       // Observar cada palavra do array.
-      if (strlen($words[$i]) < $l){
+      if (strlen($words[$i]) < $l) {
         // Ao adicionar uma palavra, já adicionamos o espaçamento.
         // (Exceto para a primeira palavra do texto).
-          if (i=0){
-            $cut.=$words[$i];}
-          else{
-            $cut.=" ".$words[$i];
-            $l -= strlen($words[$i]);
-            }
+        if ($i = 0){
+          $cut .= $words[$i];}
+        else{
+          $cut .= " ".$words[$i];
+          $l -= strlen($words[$i]);
+          }
         }
-        elseif (strlen($words[$i]) <= $length){
-            $cut.="\n".$words[$i];
-            $l = $length - strlen($words[$i]);
+        elseif (strlen($words[$i]) <= $length) {
+          $cut .= "\n".$words[$i];
+          $l = $length - strlen($words[$i]);
         }
         // Caso extremo: palavra maior que a linha:
         else {
-            if($l < $length){
-                $cut.="\n";
-            }
-            // Garantindo que essa palavra estará iniciando uma linha.
-            $parte1="";
-            for($j = 0; $j < $length; $j++){
-                $parte1.=$words[$i][$j];
-            }
-            $parte2="";
-            for($k = $length; $k < strlen($words[$i]); $k++){
-                $parte2.=$words[$i][$k];
-            }
-          if (i=0){
-            $cut.=$parte1;}
-          else{
-            $cut.=" ".$parte1;}
-          $l = 0;
-          // A palavra em questão pode ser mais extensa que 2 linhas, tendo que ser dividida em mais partes...
-          // Então para garantir que seja dividida em quantas partes necessárias, o resto da palavra volta para ser analisada novamente.
-          $words[$i]=$parte2;
-          $i--;
-        } 
+          if($l < $length) {
+            $cut .= "\n";
+          }
+          // Garantindo que essa palavra estará iniciando uma linha.
+          $parte1 = "";
+          for($j = 0; $j < $length; $j++) {
+            $parte1.=$words[$i][$j];
+          }
+          $parte2 = "";
+          for($k = $length; $k < strlen($words[$i]); $k++) {
+            $parte2 .= $words[$i][$k];
+          }
+          if ($i=0) {
+            $cut .= $parte1;
+          }
+        else {
+          $cut .= " ".$parte1;}
+        $l = 0;
+        // A palavra em questão pode ser mais extensa que 2 linhas, tendo que ser dividida em mais partes...
+        // Então para garantir que seja dividida em quantas partes necessárias, o resto da palavra volta para ser analisada novamente.
+        $words[$i]=$parte2;
+        $i--;
+      } 
     }
     $lista = explode("\n",$cut);
     return $lista;
