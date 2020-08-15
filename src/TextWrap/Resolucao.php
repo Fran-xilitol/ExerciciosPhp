0be9087f107816a55910bf2e189f9e62c0db7cb7
Fran-xilitol <55721827+Fran-xilitol@users.noreply.github.com>
2020-08-14 21:44:50 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -25,59 +25,8 @@ class Resolucao implements TextWrapInterface {
    */
   public function textWrap($text, $length): array {
 
-    $cut = "";
-    // String para vazio que será preenchido.
-    $words = explode(" ", $text);
-    // Separar as palavras do string de entrada em um array de palavras.
-    // Preenchendo o string de saída:
-    $l = $length;
-    // Espaço disponível de cada linha.
-    for ($i = 0; $i < count($words); $i++) {
-      // Observar cada palavra do array.
-      if (strlen($words[$i]) < $l) {
-        // Ao adicionar uma palavra, já adicionamos o espaçamento.
-        // (Exceto para a primeira palavra do texto).
-        if ($i = 0) {
-          $cut .= $words[$i];
-        }
-        else {
-          $cut .= " " . $words[$i];
-          $l -= strlen($words[$i]);
-        }
-      }
-      elseif (strlen($words[$i]) <= $length) {
-        $cut .= "\n" . $words[$i];
-        $l = $length - strlen($words[$i]);
-      }
-      // Caso extremo: palavra maior que a linha:
-      else {
-        if ($l < $length) {
-          $cut .= "\n";
-        }
-        // Garantindo que essa palavra estará iniciando uma linha.
-        $parte1 = "";
-        for ($j = 0; $j < $length; $j++) {
-          $parte1 .= $words[$i][$j];
-        }
-        $parte2 = "";
-        for ($k = $length; $k < strlen($words[$i]); $k++) {
-          $parte2 .= $words[$i][$k];
-        }
-        if ($i = 0) {
-          $cut .= $parte1;
-        }
-        else {
-          $cut .= " " . $parte1;
-        }
-        $l = 0;
-        // A palavra em questão pode ser mais extensa que 2 linhas...
-        // Solução: retornar o resto da palavra para a análise.
-        $words[$i] = $parte2;
-        $i--;
-      }
-    }
-    $lista = explode("\n", $cut);
-    return $lista;
+    
+    return "hiee";
   }
 
 }
